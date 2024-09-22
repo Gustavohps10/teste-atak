69ebbcad0b504372a5939d432e42943ea23832c2
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-21 23:58:17 -0300

    feat: implement User repository

---
+++ teste-atak.Domain/Contracts/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using teste_atak.Domain.Entities;
+
+namespace teste_atak.Domain.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetById(string id);
+        Task<User> GetByEmail(string email);
+        Task Insert(User user);
+    }
+}
---
+++ teste-atak.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using teste_atak.Domain.Entities;
+using teste_atak.Domain.Contracts;
+using teste_atak.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace teste_atak.Infra.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAll()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetById(string id)
+        {
+            User? user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                throw new KeyNotFoundException("Usuário não encontrado.");
+            }
+            return user;
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            User? user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email);
+            if (user == null)
+            {
+                throw new KeyNotFoundException("Usuário não encontrado.");
+            }
+            return user;
+        }
+
+        public async Task Insert(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
