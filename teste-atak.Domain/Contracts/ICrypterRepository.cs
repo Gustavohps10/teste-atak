a0113a591a7dc5807c98b30b1a30fd48b4c6c054
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-22 13:12:04 -0300

    feat: implement CrypterRepository with async encrypt and compare methods using BCrypt

---
+++ teste-atak.Domain/Contracts/ICrypterRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace teste_atak.Domain.Contracts
+{
+    public interface ICrypterRepository
+    {
+        Task<string> EncryptAsync(string plainText);
+        Task<bool> CompareAsync(string plainText, string encryptedText);
+    }
+}
---
+++ teste-atak.Infra.Data/Repositories/CrypterRepository.cs
@@ -0,0 +1,21 @@
+﻿using BCrypt.Net;
+using System.Threading.Tasks;
+using teste_atak.Domain.Contracts;
+
+namespace teste_atak.Infra.Data.Repositories
+{
+    internal class CrypterRepository : ICrypterRepository
+    {
+        public Task<string> EncryptAsync(string plainText)
+        {
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(plainText);
+            return Task.FromResult(hashedPassword);
+        }
+
+        public Task<bool> CompareAsync(string plainText, string encryptedText)
+        {
+            var isMatch = BCrypt.Net.BCrypt.Verify(plainText, encryptedText);
+            return Task.FromResult(isMatch);
+        }
+    }
+}
