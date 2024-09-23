5eaf5d74e199103b10c0dd70fd616a60efda35b1
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-23 05:00:46 -0300

    feat(api): add health controller

---
+++ teste-atak.Server/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace teste_atak.Server.Controllers
+{
+    [Route("health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return Ok(new { Status = "API está no ar!" });
+        }
+    }
+}
