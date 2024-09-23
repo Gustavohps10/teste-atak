fdc1a1814d1c59c15644a8f8ff3adb97e4b62cbe
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-09-23 04:16:05 -0300

    chore: change zod schema validation

--- teste-atak.client/src/env.ts
+++ teste-atak.client/src/env.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  VITE_API_BASE_URL: z.string().default('http://localhost:5000'),
+  VITE_API_BASE_URL: z.string(),
 })
 
 export const env = envSchema.parse(import.meta.env)
